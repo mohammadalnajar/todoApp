d4685927a2f29bd2c809d51373762be86ef07328
Mohammad <malnajjar16@gmail.com>
2021-08-13 01:06:27 +0200

    style modified

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const hbs = exphbs.create({});
 const app = express();
 const todosRouter = require('./routes/todosRouter');
 const port = 9988;
@@ -17,7 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 //  router todos
 app.use('/todos', todosRouter);
-
+hbs.handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+});
 app.get('/', (req, res) => {
   res.render('index');
 });
